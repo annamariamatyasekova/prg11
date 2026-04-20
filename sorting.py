86e994da80b3a93bd14e5d0eb40979837842090b
annamariamatyasekova <anna.maria.matyasekova@gmail.com>
2026-04-20 12:48:15 +0200

    reseni

---
+++ sorting.py
@@ -0,0 +1,19 @@
+import random
+
+def random_numbers(count, low=0, high=100):
+    return [random.randint(low, high) for _ in range(count)]
+
+
+def selection_sort(numbers):
+    numbers = numbers.copy() #povodni seznam bez zmeny
+    for pozice_ukladani in range(len(numbers)):
+        for pozice_prochazena in range(pozice_ukladani + 1, len(numbers)):
+            if numbers[pozice_prochazena] < numbers[min_idx]:
+                min_idx= pozice_prochazena
+        numbers [pozice_ukladani], numbers[min_idx] = numbers[min_idx], numbers[pozice_ukladani]
+    return numbers
+
+
+numbers =[5,1,4,2,8]
+print("Puvodni seznam:", numbers)
+print("Serazeny seznam:", selection_sort(numbers))
